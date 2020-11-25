c0b70742684e6ac59d6a1c71e71d45701e1a0bd0
LukeGall <lukepgall@gmail.com>
2020-11-25 10:32:40 +0000

    Can now upload and download puzzles

--- src/solo-project/src/app/Classes/boardPieces/board-piece.ts
+++ src/solo-project/src/app/Classes/boardPieces/board-piece.ts
@@ -31,7 +31,7 @@ export class BoardPiece {
     }
 
     imgLink: string;
-    position: Pos;
-    type:Piece;
+    position: Pos = new Pos(-1,-1);
+    type:Piece = Piece.Empty;
     locked: boolean=false;
 }
