6e3092a322d118e8a0fb1cfe46f0019d0774fa32
LukeGall <lukepgall@gmail.com>
2020-12-04 18:27:52 +0000

    Initial dist subtree commit

--- src/solo-project/src/environments/environment.prod.ts
+++ src/solo-project/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyB8jLvDuHJqKa35VrQ0pi7wiLxR5Z2Wtt0",
+    authDomain: "virtual-turing-tumble.firebaseapp.com",
+    databaseURL: "https://virtual-turing-tumble.firebaseio.com",
+    projectId: "virtual-turing-tumble",
+    storageBucket: "virtual-turing-tumble.appspot.com",
+    messagingSenderId: "288991590703",
+    appId: "1:288991590703:web:6b68cb31ecf71ac1d789da",
+    measurementId: "G-LWY3YBYB9K"
+  }
 };
