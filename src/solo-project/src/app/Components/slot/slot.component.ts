bd0ae6c981567b6b5b9b1a2594389e4f88da3ccb
LukeGall <lukepgall@gmail.com>
2021-02-05 15:02:28 +0000

    Ghost Image on hover

--- src/solo-project/src/app/Components/slot/slot.component.ts
+++ src/solo-project/src/app/Components/slot/slot.component.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Marble } from 'src/app/Classes/boardParts/marble';
 import { Pos } from 'src/app/Classes/boardParts/pos';
 import { Slot } from 'src/app/Classes/boardParts/slot';
+import { Piece } from 'src/app/Classes/piece.enum';
 
 @Component({
   selector: 'app-slot',
@@ -13,7 +14,10 @@ export class SlotComponent implements OnInit {
   @Input() x: number;
   @Input() y: number;
   @Input() marble: Marble;
+  @Input() heldPiece: Piece;
   @Output() slotClicked: EventEmitter<Pos> = new EventEmitter<Pos>();
+
+  hover = false;
 
   constructor() { }
   ngOnInit(): void {
@@ -21,6 +25,19 @@ export class SlotComponent implements OnInit {
 
   click() {
     this.slotClicked.emit(new Pos(this.x, this.y));
+  }
+
+  shouldHover(){
+    if (this.slot.partName == "CompSlot"){
+      return (this.heldPiece && this.heldPiece != Piece.Delete);
+    } else {
+      return (this.heldPiece == Piece.Gear);
+    }
+  }
+
+  getPieceImage(): string{
+    if(this.heldPiece == Piece.GearBit) return "assets/gear-bit.svg";
+    return "assets/"+this.heldPiece.toLowerCase()+".svg";
   }
 
   isMarbleFall(){
